16d0f41bea68280bbc47a21528c9da90a5913ba1
ZoroIsMyMain <37275050+ZoroIsMyMain@users.noreply.github.com>
2019-04-28 21:55:57 -0300

    Fisrt Up proyect ArtExperience

---
+++ Art Experience/src/main/java/MainAplication.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class MainAplication {
+		public static void main(String[] args) throws Exception {
+			SpringApplication.run(MainAplication.class, args);
+		}
+	}
+
+
