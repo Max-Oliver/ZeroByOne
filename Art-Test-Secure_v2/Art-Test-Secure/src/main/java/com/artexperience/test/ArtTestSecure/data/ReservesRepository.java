aa61a9627f30aee0522460d67d5e558d79b91686
ZoroIsMyMain <37275050+ZoroIsMyMain@users.noreply.github.com>
2019-07-26 02:02:00 -0300

    Delete ReservesRepository.java

--- Art-Test-Secure_v2/Art-Test-Secure/src/main/java/com/artexperience/test/ArtTestSecure/data/ReservesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.artexperience.test.ArtTestSecure.data;
-
-import com.artexperience.test.ArtTestSecure.model.Reserve;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservesRepository extends JpaRepository<Reserve, Long> {
-}
