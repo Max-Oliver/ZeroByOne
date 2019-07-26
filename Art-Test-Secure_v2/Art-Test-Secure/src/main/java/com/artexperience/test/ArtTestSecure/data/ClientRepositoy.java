e632f86d9513be8bcb2cb48b4405f7aa9fea834f
ZoroIsMyMain <37275050+ZoroIsMyMain@users.noreply.github.com>
2019-07-26 02:01:54 -0300

    Delete ClientRepositoy.java

--- Art-Test-Secure_v2/Art-Test-Secure/src/main/java/com/artexperience/test/ArtTestSecure/data/ClientRepositoy.java
+++
@@ -1,7 +0,0 @@
-package com.artexperience.test.ArtTestSecure.data;
-
-import com.artexperience.test.ArtTestSecure.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepositoy extends JpaRepository<Client,Long> {
-}
