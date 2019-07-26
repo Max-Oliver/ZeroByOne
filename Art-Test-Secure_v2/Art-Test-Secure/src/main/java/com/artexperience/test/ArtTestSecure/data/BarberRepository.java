4f0979b4647c82d64eca72b3f23da7deb6716098
ZoroIsMyMain <37275050+ZoroIsMyMain@users.noreply.github.com>
2019-07-26 02:01:48 -0300

    Delete BarberRepository.java

--- Art-Test-Secure_v2/Art-Test-Secure/src/main/java/com/artexperience/test/ArtTestSecure/data/BarberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.artexperience.test.ArtTestSecure.data;
-
-import com.artexperience.test.ArtTestSecure.model.Barber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarberRepository extends JpaRepository<Barber, Long> {
-}
